ede96123fa26ebf78371cccb0d1c508553d190fe
Kaan Avci <kaanyewest1@gmail.com>
2020-07-01 23:49:42 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,6 @@ from suppFunctions import *
 from patientGenerator import generatePatient
 
 
-
 # sample usage
 
 def main():
@@ -19,21 +18,22 @@ def main():
         # morning and evening dose
         dosageEvents=[
             # 7 AM, 40% adherence, within 100 minutes
-            DosageEvent(420, 0.4, 100),
+            DosageEvent(420, 0.2, 100),
 
-            # 6 PM, 50% adherence, within 60 minutes
-            DosageEvent(1080, 0.5, 60)
+            # 6 PM, 20% adherence, within 60 minutes
+            DosageEvent(1080, 0.2, 60)
         ],
         # adherence that is above average during the week
         # and below average on the weekend,
-        weeklyAdherence=[0, 0.25, 0.25, 0.25, 0.25, -0.25, -0.25],
+        weeklyAdherence=[0.25, 0.25, 0.25, 0.25, 0.25, -0.25, 0.25],
         # adherence improving each week, more slowly over time
         adherenceTrend=logGrowth)
 
+
     expectedTimes, actualTimes, dates, originalTimes = generateCoordinates(df)
     ticks = calculateXTicks(start, end)
-
-    plotAdherence(dates, expectedTimes, actualTimes, originalTimes, ticks)
+    caption = createCaption([DosageEvent(420, 0.2, 100), DosageEvent(900, 0.4, 50)], [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25] )
+    plotAdherence(dates, expectedTimes, actualTimes, originalTimes, ticks, df['adherence'].to_list(), caption)
 
 if __name__ == "__main__":
     main()
